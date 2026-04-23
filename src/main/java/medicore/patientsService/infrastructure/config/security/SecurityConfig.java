ac18dd842b43074f0cd4106cd9b0d524e6f3c5f2
bany-ph <bpuertaherrera@gmail.com>
2026-04-23 17:15:23 -0500

    Integrate JWT authentication with custom `JwtAuthenticationConverter` and enhance `SecurityConfig` for stateless session management.

--- src/main/java/medicore/patientsService/infrastructure/config/security/SecurityConfig.java
+++ src/main/java/medicore/patientsService/infrastructure/config/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package medicore.patientsService.infrastructure.config.security;
 
+import lombok.RequiredArgsConstructor;
 import medicore.patientsService.infrastructure.config.keycloak.KeycloakProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -8,13 +9,18 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableMethodSecurity
+@EnableMethodSecurity // for roles
 @EnableWebSecurity
 @EnableConfigurationProperties(KeycloakProperties.class)
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationConverter jwtAuthenticationConverter;
+
 
     @Bean
     public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
@@ -23,9 +29,19 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize ->
                         authorize.requestMatchers("/register").permitAll()
                                 .anyRequest().authenticated()
-                );
+                )
+                .oauth2ResourceServer(
+                        oauth -> oauth.jwt(
+                                jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)
+                        )
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
 
         return http.build();
     }
+
+
+
+
 }
